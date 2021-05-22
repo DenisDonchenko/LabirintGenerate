c98e6f441435a80b27e2f17b05b2cb772773136f
”Denis <donchenkodenis2000@gmail.com>
2021-05-22 14:39:27 +0300

    Start_project

---
+++ src/main/java/com/example/springlabirints/SpringLabirintsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlabirints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabirintsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLabirintsApplication.class, args);
+    }
+
+}
